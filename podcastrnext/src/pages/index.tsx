96baa295bd3f74ce2fc942e3e12e22ea699f2ffa
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-04-21 12:48:20 -0300

    Commit inicial

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,8 +1,28 @@
-import React from "react";
-import Header from "../components/Header";
+// SPA
+// SSR
+// SSG -> só funciona em produção
 
-export default function Home() {
+
+export default function Home(props) {
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+
   )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes') // sempre observar o http e https
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // tempo para cada versão gerada da página
+  }
 }
